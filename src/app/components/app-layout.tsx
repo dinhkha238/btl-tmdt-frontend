e61e79f2b4caf24248072cd3b576743f8d574d52
kha <nguyendinhkha.s4c.@gmail.com>
2023-08-07 10:30:17 +0700

    bonus

--- src/app/components/app-layout.tsx
+++ src/app/components/app-layout.tsx
@@ -1,10 +1,10 @@
-import { Navigate, Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import { AppHeader } from "./app-header";
 import { AppFooter } from "./app-footer";
 
 export const AppLayout: React.FC = () => {
   // const isAuthenticated = localStorage.getItem("token");
-  const a = localStorage.getItem("token");
+  // const a = localStorage.getItem("token");
   return (
     <>
       {/* {a == "true" ? ( */}
